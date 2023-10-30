79c5d90ec888e56dfec505343809b0ae3f862c26
Jarhead <j.a.lindsay06@gmail.com>
2023-10-30 21:35:36 +1000

    square test update

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SquareTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SquareTest.java
@@ -1,19 +1,34 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.acmerobotics.dashboard.config.Config;
 import com.arcrobotics.ftclib.command.SequentialCommandGroup;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
 import org.firstinspires.ftc.teamcode.commands.DriveDistance;
 import org.firstinspires.ftc.teamcode.commands.OpModeTemplate;
-
+import org.firstinspires.ftc.teamcode.commands.RotateCommand;
+@Config
+@Autonomous
 public class SquareTest extends OpModeTemplate {
+
+    public static double distance = 50;
+    public static double angle = 90;
     @Override
     public void initialize() {
         initHardware(true);
         schedule(new SequentialCommandGroup(
-                new DriveDistance(50, 1,0,0.5, drive),
-                new DriveDistance(50, 0,1,0.5, drive),
-                new DriveDistance(50, -1,0,0.5, drive),
-                new DriveDistance(50, 0,-1,0.5, drive)
+                new DriveDistance(distance, 1,0,0.5, drive),
+                new DriveDistance(distance, 0,1,0.5, drive),
+                new DriveDistance(distance, -1,0,0.5, drive),
+                new DriveDistance(distance, 0,-1,0.5, drive),
+                new DriveDistance(distance, 1,0,0.5, drive),
+                new RotateCommand(angle, 1, 0.5, drive),
+                new DriveDistance(distance, 1,0,0.5, drive),
+                new RotateCommand(angle, 1, 0.5, drive),
+                new DriveDistance(distance, 1,0,0.5, drive),
+                new RotateCommand(angle, 1, 0.5, drive),
+                new DriveDistance(distance, 1,0,0.5, drive),
+                new RotateCommand(angle, 1, 0.5, drive)
         ));
     }
 }
